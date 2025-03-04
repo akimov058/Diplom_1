dd21f0d5982f39f8b765062a901ef0c2cf1a18b5
Akimov058 <YuAkimov.beeline@yandex.ru>
2025-03-04 22:52:54 +0300

    Реализовал тест test_get_name_ingredient

--- tests/bun_ingredient.py
+++ tests/bun_ingredient.py
@@ -6,3 +6,7 @@ class TestIngredient:
         ingredient = Ingredient(Data.INGREDIENT_TYPE,Data.INGREDIENT_NAME,Data.INGREDIENT_PRICE)
         assert ingredient.get_price() == Data.INGREDIENT_PRICE
 
+    def test_get_name_ingredient(self):
+        ingredient = Ingredient(Data.INGREDIENT_TYPE,Data.INGREDIENT_NAME,Data.INGREDIENT_PRICE)
+        assert ingredient.get_name() == Data.INGREDIENT_NAME
+
