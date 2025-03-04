d89a951f5376e04c67c71202a896953684415fd8
Akimov058 <YuAkimov.beeline@yandex.ru>
2025-03-04 22:43:04 +0300

    Сделал тест test_get_price, который проверяет метод get_price

--- tests/bun_test.py
+++ tests/bun_test.py
@@ -6,3 +6,7 @@ class TestBun:
     def test_get_name_bun(self):
         bun = Bun(Data.BUN_NAME,Data.BUN_PRICE)
         assert bun.get_name() == Data.BUN_NAME
+
+    def test_get_price(self):
+        bun = Bun(Data.BUN_NAME,Data.BUN_PRICE)
+        assert bun.get_price() == Data.BUN_PRICE
