a1c2c0e54ed8872b27b8bdee27794a3ddd2219c4
Akimov058 <YuAkimov.beeline@yandex.ru>
2025-03-05 21:35:52 +0300

    Написал тест test_available_buns

--- tests/database_test.py
+++ tests/database_test.py
@@ -5,4 +5,7 @@ class TestDatabase:
     @pytest.mark.parametrize('result',['black bun','white bun','red bun'])
     def test_available_buns(self,result):
         db = Database()
-        db_list = db.available_buns()
+        buns = db.available_buns()
+        buns_list = [bun.get_name() for bun in buns]
+        assert result in buns_list, buns_list
+
