4543cb364b3b4fc1ab5f7c913fce7303d0fa68e4
Akimov058 <YuAkimov.beeline@yandex.ru>
2025-03-05 21:45:13 +0300

    Написал тест test_available_ingredients

--- tests/database_test.py
+++ tests/database_test.py
@@ -9,3 +9,10 @@ class TestDatabase:
         buns_list = [bun.get_name() for bun in buns]
         assert result in buns_list, buns_list
 
+    @pytest.mark.parametrize('result',['hot sauce','sour cream','chili sauce','cutlet','dinosaur','sausage'])
+    def test_available_ingredients(self,result):
+        db = Database()
+        ingredients = db.available_ingredients()
+        ingredients_list = [i.get_name() for i in ingredients]
+        assert result in ingredients_list, ingredients_list
+
